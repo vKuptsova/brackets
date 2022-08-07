10008ed0f89be8a642b3c0d9d478741fd53ff065
vKuptsova <kuptsowa.vika@gmail.com>
2022-08-08 01:08:29 +0300

    feat: add solution in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let obj = {};
+  let arr = [];
+  for (let i = 0; i<bracketsConfig.length; i++) {
+    obj[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+  for (let j = 0; j<str.length; j++) {
+    for (let key in obj) {
+      if (obj[key] === str[j]) {
+        arr.push(str[j]);
+      }
+    }
+    let el = arr[arr.length-1];
+    if (obj[str[j]] === el) {
+      arr.pop();
+    }
+  }
+  return arr.length === 0;   
 }
